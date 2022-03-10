04194a819f337b05a8bc9623640b38f451432efe
Matt J Hampson <mjhampson@gmail.com>
2022-03-10 15:45:40 +0000

    remove tagline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,6 @@ export default function Header() {
         <Link to='/' className="Nav__Link">
           <h1 id="main-title">Northcoders News</h1>
         </Link>
-          
-          <h2 id="tagline">“Bringing you best fake news since 2022”</h2>
 
        <Link to='/user'>
           <h3 className="nav-tags">User Log-in</h3>
